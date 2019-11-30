0ba25f4bb05620050712c5e82a152a2f191036da
Juan Pablo Vivas Reinoso <jvivas@merqueo.com>
2019-11-29 21:17:15 -0500

    [Progress] Lambdas

--- app/src/main/java/com/example/trainigkotlin/MainActivity.kt
+++ app/src/main/java/com/example/trainigkotlin/MainActivity.kt
@@ -5,20 +5,21 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import org.jetbrains.anko.startActivity
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
 
     private val adapter = MediaAdapter { (id) -> navigateToDetail((id)) }
+    private lateinit var job: Job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recycler.adapter = adapter
         MediaProvider.dataAsync { updateData(it) }
+        job = Job()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -37,11 +38,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         filter?.let {
-
-            CoroutineScope(Dispatchers.IO).launch {
-                val media1 = MediaProvider.dataSync("cats")
-                val media2 = MediaProvider.dataSync("nature")
-                updateData(media1 + media2)
+            CoroutineScope(Dispatchers.Main).launch {
+                val media1 = async(Dispatchers.IO) { MediaProvider.dataSync("cats") }
+                val media2 = async(Dispatchers.IO) { MediaProvider.dataSync("nature") }
+                updateData(media1.await() + media2.await())
             }
         }
 
@@ -59,4 +59,11 @@ class MainActivity : AppCompatActivity() {
         startActivity<DetailActivity>(DetailActivity.ID to id)
     }
 
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main+ job
+
+    override fun onDestroy() {
+        job.cancel()
+        super.onDestroy()
+    }
 }
