d81c9be58dba1f76f139684fca17dbe3028fb163
jvivas <jvivas@merqueo.com>
2020-01-23 23:59:39 -0500

    Commit end

--- app/src/main/java/com/example/trainigkotlin/MainActivity.kt
+++ app/src/main/java/com/example/trainigkotlin/MainActivity.kt
@@ -5,9 +5,16 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 import org.jetbrains.anko.startActivity
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.resume
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+    private lateinit var job: Job
 
     private val adapter = MediaAdapter { (id) -> navigateToDetail((id)) }
 
@@ -17,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         recycler.adapter = adapter
         progressBar.show()
         MediaProvider.dataAsync { loadFilter(it) }
+        job = Job()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -35,11 +43,27 @@ class MainActivity : AppCompatActivity() {
         }
 
         filter?.let {
-            MediaProvider.dataAsync { media -> loadFilter(media, filter) }
+
+            GlobalScope.launch(Dispatchers.Main) {
+                progressBar.show()
+                val media1 =
+                    async(Dispatchers.IO, CoroutineStart.LAZY) { MediaProvider.dataSync("animals") }
+                val media2 =
+                    async(Dispatchers.IO, CoroutineStart.LAZY) { MediaProvider.dataSync("animals") }
+                loadFilter(media1.await() + media2.await(), filter)
+                progressBar.hide()
+            }
+
             return true
 
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private suspend fun suspendeing(): List<MediaItem> = suspendCancellableCoroutine { continuation ->
+        MediaProvider.dataAsync { media ->
+            continuation.resume(media)
+        }
     }
 
     private fun loadFilter(media: List<MediaItem>, filter: Filter = Filter.None) {
@@ -54,4 +78,8 @@ class MainActivity : AppCompatActivity() {
         startActivity<DetailActivity>(DetailActivity.ID to id)
     }
 
+    override fun onDestroy() {
+        job.cancel()
+        super.onDestroy()
+    }
 }
--- app/src/main/java/com/example/trainigkotlin/MediaProvider.kt
+++ app/src/main/java/com/example/trainigkotlin/MediaProvider.kt
@@ -5,26 +5,30 @@ import org.jetbrains.anko.uiThread
 
 object MediaProvider {
 
-    private const val thumbBase = "http://placeimg.com/640/480/animals/"
+    private const val thumbBase = "http://placeimg.com/640/480/"
 
     private var data = emptyList<MediaItem>()
 
-    fun dataAsync(callBack: (List<MediaItem>) -> Unit) {
+    fun dataAsync(type: String = "animals",callBack: (List<MediaItem>) -> Unit) {
         doAsync {
             if (data.isEmpty()) {
-                Thread.sleep(4000)
-                data = (1..10).map {
-                    MediaItem(
-                        it,
-                        "Title $it",
-                        thumbBase.plus(it),
-                        if (it % 3 == 0) MediaItem.Type.VIDEO else MediaItem.Type.PHOTO
-                    )
-                }
+                dataSync(type)
             }
             uiThread {
                 callBack(data)
             }
+        }
+    }
+
+    fun dataSync(type: String): List<MediaItem>{
+        Thread.sleep(4000)
+        return (1..10).map {
+            MediaItem(
+                it,
+                "Title $it",
+                thumbBase.plus(it),
+                if (it % 3 == 0) MediaItem.Type.VIDEO else MediaItem.Type.PHOTO
+            )
         }
     }
 }
